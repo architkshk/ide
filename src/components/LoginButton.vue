3fd12068407ad3b43de405711fa07bbf5b12041c
Archit Kaushik <asarchit65@gmail.com>
2018-06-16 07:26:53 +0530

    log

---
+++ src/components/LoginButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button>LoginButton</button>
+</template>
+
+<script>
+    import Vue from 'vue'
+    export default {
+        name : 'loginbutton',
+        data() {
+            return {
+                loginUrl = `${env.oneauthURL}/oauth/authorize?response_type=code&client_id=${env.clientId}&redirect_uri=${env.publicUrl}`
+            }
+        },
+        methods : {
+            logIn () {
+                window.location.href = this.loginUrl
+            }
+            logOut () {
+
+            }
+        }
+    }
+</script>
